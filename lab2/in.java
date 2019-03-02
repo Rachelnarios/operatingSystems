4fbde4f1e148e10f8834c6313b4a3b115b89c166
Rachelnarios <rnr266@Nyu.edu>
2019-03-02 12:48:20 -0500

    Skeleton errors

--- lab2/in.java
+++ lab2/in.java
@@ -1,5 +1,7 @@
 import java.util.*;
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class in {
 // define lists
@@ -8,6 +10,11 @@ static String wait = "f";
 static String next = "f";
 static boolean verb = false;
 static String file = "";
+static Scanner randomx;
+
+Queue<Integer> blocked = new LinkedList<>();
+
+
   public static void main(String[] args) throws FileNotFoundException {
       if (args[0].equals("--verbose")) {
           verb = true;
@@ -22,22 +29,37 @@ static String file = "";
   public static void readFile() throws FileNotFoundException{
     //Read File
     Scanner f = new Scanner(new File(file));
-    Scanner random = new Scanner(new File("random-numbers.txt"));
+    randomx = new Scanner(new File("random-numbers.txt"));
     int cpuTime = 0;
+    int blockedTime = 0;
     //Create lists for each algo being parsed gloval
   //  List<E> scanItems = new ArrayList<E>();
   }
+
   public static void RR(){
+    System.out.println("Round Robin:");
     int time  = 2;
   }
   public static void FCFS(){
+  System.out.println("First come first served:");
 
   }
   public static void uni(){
+    System.out.println("Uni scheduling:");
 
   }
-  public static void short(){
-    
+  public static void shortjob(){
+  System.out.println("Short job first:");
+
+  }
+  public static void printSummary(){
+    System.out.println("Summary:");
+  }
+  public static void printVerbose() {
+    System.out.println("VERBOSE Summary:");
+  }
+  public static int RandomOS(int U){
+    return 1 + (randomx.nextInt() % U);
   }
 
 }
